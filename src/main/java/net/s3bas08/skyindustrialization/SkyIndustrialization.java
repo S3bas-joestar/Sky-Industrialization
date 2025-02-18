2f4061bacd8fa85486e7a5322a8eb2783e6ee11e
Rebonica <secundsebas@gmail.com>
2025-02-18 18:30:32 -0500

    Added the Wooden Gear and its recip

--- src/main/java/net/s3bas08/skyindustrialization/SkyIndustrialization.java
+++ src/main/java/net/s3bas08/skyindustrialization/SkyIndustrialization.java
@@ -1,5 +1,6 @@
 package net.s3bas08.skyindustrialization;
 
+import net.s3bas08.skyindustrialization.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -73,7 +74,6 @@ public class SkyIndustrialization
     {
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
-
         // Register the Deferred Register to the mod event bus so blocks get registered
         BLOCKS.register(modEventBus);
         // Register the Deferred Register to the mod event bus so items get registered
@@ -85,6 +85,8 @@ public class SkyIndustrialization
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
+
+        ModItems.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -109,8 +111,8 @@ public class SkyIndustrialization
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
-            event.accept(EXAMPLE_BLOCK_ITEM);
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+            event.accept(ModItems.GEAR);
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/s3bas08/skyindustrialization/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.s3bas08.skyindustrialization.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.s3bas08.skyindustrialization.SkyIndustrialization;
+
+public class ModItems {
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(SkyIndustrialization.MODID);
+
+    public static final DeferredItem<Item> GEAR = ITEMS.register("wooden_gear",
+            () -> new Item(new Item.Properties()));
+
+
+    public  static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
